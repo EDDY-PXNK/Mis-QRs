447bdfcc2cd35e92d4b83d1573cc4d05db408418
EDDY-PXNK <114305394+EDDY-PXNK@users.noreply.github.com>
2022-10-13 18:10:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbzuXe3WhDAKig26bnjOPVVxCXQWQJyUjg4EODxXmfv4a6MTTdVt4qpEKUtKXebwEFnM/exec";
