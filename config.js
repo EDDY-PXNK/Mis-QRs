b81c06c2cd07be02185336da8937d8009ed84675
EDDY-PXNK <114305394+EDDY-PXNK@users.noreply.github.com>
2022-10-13 19:03:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
 const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbzuXe3WhDAKig26bnjOPVVxCXQWQJyUjg4EODxXmfv4a6MTTdVt4qpEKUtKXebwEFnM/exec";
+
